dd382dac652ce1f343aeeca5e1d5a5312dd8739c
sly <sylvaincadoret@hotmail.com>
2024-09-06 10:06:35 +0200

    first step TechApp

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,8 +3,7 @@ import "./style.scss";
 import { NavLink } from "react-router-dom";
 
 function Header() {
-  const link1 = "https://www.google.fr";
-  const link2 = "https://www.youtube.com";
+
 
   // Creation fonction menu Burger
   let isBurgerOpen = false;
@@ -33,20 +32,7 @@ function Header() {
             <li>
               <NavLink to="/page4">Page4</NavLink>
             </li>
-            <span>
-              <a href={link1} target="_blank" rel="noopener noreferrer">
-                <img
-                  src="https://img.icons8.com/?size=64&id=52539&format=png"
-                  alt="github"
-                />
-              </a>
-              <a href={link2} target="_blank" rel="noopener noreferrer">
-                <img
-                  src="https://img.icons8.com/fluent/48/000000/linkedin.png"
-                  alt="linkedin"
-                />
-              </a>
-            </span>
+      
           </ul>
           <div className="header__burgerMenu" onClick={burgerToggle}></div>
         </nav>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,18 @@
-import "./style.scss";
+import styles from "./style.module.scss";
 // import { useState, useEffect } from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Homepage() {
-  
   return (
-    <div className="homepage">
-      <h1>Homepage</h1>
+    <div className={styles.homepageContainer}>
+      <img src="assets/logo-pix-dark.png" alt="" />
+      <h1>Tech-App</h1>
+
+      <div className={styles.categories}>
+        <Link to="/page2">Fiches missions</Link>
+        <Link to="/page3">Rapports d'interventions</Link>
+        <Link to="/page4">Fiches incidents</Link>
+      </div>
     </div>
   );
 }
