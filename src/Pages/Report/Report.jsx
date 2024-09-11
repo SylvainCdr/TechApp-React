77626cd6ef899de147d020405de3b308f61438ca
sly <sylvaincadoret@hotmail.com>
2024-09-11 17:01:04 +0200

    backup

--- src/Pages/Report/Report.jsx
+++ src/Pages/Report/Report.jsx
@@ -1,15 +1,15 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { db } from "../../firebase/firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, getDocs, collection } from "firebase/firestore";
 import styles from "./style.module.scss";
 
 export default function InterventionReport() {
-  
   const { reportId } = useParams(); // Récupérer l'ID du rapport depuis l'URL
   const [report, setReport] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [technicians, setTechnicians] = useState([]);
 
   // Fonction pour récupérer un rapport d'intervention spécifique depuis Firestore
   const fetchReport = async () => {
@@ -30,8 +30,23 @@ export default function InterventionReport() {
     }
   };
 
+   // Fonction pour récupérer les techniciens depuis Firestore
+   const fetchTechnicians = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, "technicians"));
+      const techniciansList = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        name: doc.data().firstName + " " + doc.data().lastName,
+        urlPhoto: doc.data().urlPhoto,
+      }));
+      setTechnicians(techniciansList);
+    } catch (error) {
+      console.error("Erreur lors de la récupération des techniciens : ", error);
+    }
+  };
   useEffect(() => {
     fetchReport();
+    fetchTechnicians();
   }, [reportId]);
 
   if (loading) {
@@ -44,26 +59,66 @@ export default function InterventionReport() {
 
   return (
     <div className={styles.reportContainer}>
-
       <h1> Rapport d'intervention N° {report.id} </h1>
       {report ? (
         <div className={styles.reportItem}>
-          <h2> {report.createdAt?.toDate().toLocaleDateString()} - {report.client.nomEntreprise}</h2>
-       
-          <h3>Site : {report.site.adresse}</h3>
-          <h3>Intervenant : {report.intervenant}</h3>
-          <p>Actions menées :   {report.actionsDone.map((action, index) => (
-          <li key={index}>{action.description}</li>
-        ))}</p>
-          <p>Remarques : {report.remarques.map((remarque, index) => (
-          <li key={index}>{remarque.remarque}</li>
-        ))}</p>
-          <p>Risques : {report.risques ? "Oui" : "Non"}</p>
+          <h2>
+            {" "}
+            {report.interventionDate} - {report.client.nomEntreprise}
+          </h2>
+
+          <div className={styles.section1}>
+            <div className={styles.section1Left}>
+              <h4>Entreprise / Site</h4>
+              <ul>
+                <li>Client : {report.client.nomEntreprise}</li>
+                <li>Email : {report.client.email}</li>
+                <li>Téléphone : {report.client.tel}</li>
+                <li>Adresse du site : {report.site.adresse}</li>
+                <li>Nom du contact sur site : {report.site.nomContact}</li>
+                <li>Fonction du contact : {report.site.fonctionContact}</li>
+                <li>Téléphone du contact: {report.site.telContact}</li>
+              </ul>
+            </div>
+
+            <div className={styles.section1Right}>
+              <h4>Intervenant</h4>
+              <h3> {report.intervenant}</h3>
+             
+              <img src={technicians.find((tech) => tech.name === report.intervenant)?.urlPhoto} alt="Photo de l'intervenant" />
+
+            </div>
+            </div>
+
+          <div className={styles.section2}>
+            <div className={styles.section2Left}>
+              <h4>Actions menées </h4>
+              {report.actionsDone.map((action, index) => (
+                <li key={index}> <i class="fa-solid fa-check"></i> {action.description}</li>
+              ))}
+            </div>
+
+            <div className={styles.section2Right}>
+              <h4> Remarques</h4>
+            {report.remarques.map((remarque, index) => (
+              <li key={index}> <i class="fa-solid fa-minus"></i>{remarque.remarque}</li>
+            ))}
+
+            <p>  <i class="fa-solid fa-triangle-exclamation"></i> Intervention à risque : {report.risques ? "Oui" : "Non"}</p>
+            </div>
+          </div>
+         
+
           <div>
             <h4>Photos :</h4>
             <div className={styles.photosContainer}>
               {report.photos?.map((photoUrl, index) => (
-                <img key={index} src={photoUrl} alt={`Photo ${index + 1}`} className={styles.photo} />
+                <img
+                  key={index}
+                  src={photoUrl}
+                  alt={`Photo ${index + 1}`}
+                  className={styles.photo}
+                />
               ))}
             </div>
           </div>
